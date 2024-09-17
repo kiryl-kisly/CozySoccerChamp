eb10e2c50fb8b27a4b500558402b6c8312b5cd5b
Kiryl Kisly <kiryl.kisly@gmail.com>
2024-09-17 18:33:08 +0300

    fix: fix url

--- src/Client/react-web-app/src/services/AxiosInstance.ts
+++ src/Client/react-web-app/src/services/AxiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const baseURL = 'http://localhost:8000/api/'
+const baseURL = 'api/'
 
 const axiosClient = (): AxiosInstance => {
   const headers = {
