7c1f1862b4094c498484df149cba1366bf3dafc8
Kiryl Kisly <kiryl.kisly@gmail.com>
2025-01-23 22:16:19 +0300

    feat: Improve data update in PointCalculatingJob parallele loop execution

--- src/Backend/Server/CozySoccerChamp.Infrastructure/BackgroundServices/Jobs/PointCalculatingJob.cs
+++ src/Backend/Server/CozySoccerChamp.Infrastructure/BackgroundServices/Jobs/PointCalculatingJob.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using CozySoccerChamp.Infrastructure.BackgroundServices.Jobs.Settings;
 using Quartz;
@@ -34,58 +35,60 @@ public class PointCalculatingJob(
 
     private async Task ExecuteAsync()
     {
-        var finishedMatches = await GetFinishedMatchesAsync();
-
+        var finishedMatches = await GetFinishedMatchesDictAsync();
         if (finishedMatches.Count == 0)
             return;
 
         var users = await GetUsersWithPredictionsAsync(finishedMatches);
-
         if (users.Count == 0)
             return;
 
         var usersToUpdate = ProcessPredictions(users, finishedMatches);
-
         if (usersToUpdate.Count != 0)
             await userRepository.UpdateRangeAsync(usersToUpdate);
     }
 
-    private async Task<List<MatchResult>> GetFinishedMatchesAsync()
+    private async Task<Dictionary<int, MatchResult>> GetFinishedMatchesDictAsync()
     {
         return await matchResultRepository.GetAllAsQueryable(asNoTracking: true, x => x.Match)
             .Where(x => x.Status == MatchResultStatus.Finished)
-            .ToListAsync();
+            .ToDictionaryAsync(x => x.Match.Id);
     }
 
-    private async Task<List<ApplicationUser>> GetUsersWithPredictionsAsync(List<MatchResult> finishedMatches)
+    private async Task<List<ApplicationUser>> GetUsersWithPredictionsAsync(Dictionary<int, MatchResult> finishedMatchesDict)
     {
         return await userRepository.GetAllAsQueryable(includes: x => x.Predictions)
-            .Where(x => x.Predictions.Any(p => finishedMatches.Select(m => m.MatchId).Contains(p.MatchId)))
+            .Where(x => x.Predictions.Any(p => finishedMatchesDict.Select(m => m.Key).Contains(p.MatchId)))
             .ToListAsync();
     }
 
-    private List<ApplicationUser> ProcessPredictions(List<ApplicationUser> users, List<MatchResult> finishedMatches)
+    private List<ApplicationUser> ProcessPredictions(List<ApplicationUser> users, Dictionary<int, MatchResult> finishedMatchesDict)
     {
-        var usersToUpdate = new List<ApplicationUser>();
-
-        foreach (var user in users)
+        var usersToUpdate = new ConcurrentDictionary<long, ApplicationUser>();
+        
+        Parallel.ForEach(users, user =>
         {
-            foreach (var prediction in user.Predictions)
-            {
-                var matchResult = finishedMatches.FirstOrDefault(x => x.MatchId == prediction.MatchId);
+            var predictionsToUpdate = new ConcurrentBag<Prediction>();
 
-                if (matchResult is null)
-                    continue;
+            Parallel.ForEach(user.Predictions, prediction =>
+            {
+                if (!finishedMatchesDict.TryGetValue(prediction.MatchId, out var matchResult))
+                    return;
 
                 prediction.PointPerMatch = GetUserPoint(matchResult, prediction);
                 prediction.Coefficient = GetCoefficient(matchResult);
+                predictionsToUpdate.Add(prediction);
+            });
 
-                if (!usersToUpdate.Contains(user))
-                    usersToUpdate.Add(user);
-            }
-        }
+            if (predictionsToUpdate.IsEmpty)
+                return;
 
-        return usersToUpdate;
+            user.Predictions = predictionsToUpdate.ToList();
+
+            usersToUpdate.TryAdd(user.TelegramUserId, user);
+        });
+
+        return usersToUpdate.Values.ToList();
     }
 
     private int GetUserPoint(MatchResult matchResult, Prediction prediction)
@@ -137,14 +140,14 @@ public class PointCalculatingJob(
             var regularTime = matchResult.RegularTime.Split(":");
             homeTeamScore = int.Parse(regularTime[0]);
             awayTeamScore = int.Parse(regularTime[1]);
-            
+
             return (homeTeamScore, awayTeamScore);
         }
 
         var fullTime = matchResult.FullTime.Split(":");
         homeTeamScore = int.Parse(fullTime[0]);
         awayTeamScore = int.Parse(fullTime[1]);
-        
+
         return (homeTeamScore, awayTeamScore);
     }
 }
