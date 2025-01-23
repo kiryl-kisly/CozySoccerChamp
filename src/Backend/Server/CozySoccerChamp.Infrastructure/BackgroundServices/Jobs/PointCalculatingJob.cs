604363e31404693981965b2b4120c2ef6e3bf530
Kiryl Kisly <kiryl.kisly@gmail.com>
2025-01-23 21:33:24 +0300

    feat: Improve the updating of data in PointCalculatingJob; Update entities range

--- src/Backend/Server/CozySoccerChamp.Infrastructure/BackgroundServices/Jobs/PointCalculatingJob.cs
+++ src/Backend/Server/CozySoccerChamp.Infrastructure/BackgroundServices/Jobs/PointCalculatingJob.cs
@@ -34,91 +34,117 @@ public class PointCalculatingJob(
 
     private async Task ExecuteAsync()
     {
-        var finishedMatches = await matchResultRepository.GetAllAsQueryable(asNoTracking: true, x => x.Match)
-            .Where(x => x.Status == MatchResultStatus.Finished)
-            .ToListAsync();
+        var finishedMatches = await GetFinishedMatchesAsync();
 
         if (finishedMatches.Count == 0)
             return;
 
-        foreach (var matchResult in finishedMatches)
-        {
-            await UpdatePointsForUserAsync(matchResult);
-        }
+        var users = await GetUsersWithPredictionsAsync(finishedMatches);
+
+        if (users.Count == 0)
+            return;
+
+        var usersToUpdate = ProcessPredictions(users, finishedMatches);
+
+        if (usersToUpdate.Count != 0)
+            await userRepository.UpdateRangeAsync(usersToUpdate);
     }
 
-    private async Task UpdatePointsForUserAsync(MatchResult matchResult)
+    private async Task<List<MatchResult>> GetFinishedMatchesAsync()
     {
-        var users = await userRepository.GetAllAsync(includes: x => x.Predictions);
+        return await matchResultRepository.GetAllAsQueryable(asNoTracking: true, x => x.Match)
+            .Where(x => x.Status == MatchResultStatus.Finished)
+            .ToListAsync();
+    }
+
+    private async Task<List<ApplicationUser>> GetUsersWithPredictionsAsync(List<MatchResult> finishedMatches)
+    {
+        return await userRepository.GetAllAsQueryable(includes: x => x.Predictions)
+            .Where(x => x.Predictions.Any(p => finishedMatches.Select(m => m.MatchId).Contains(p.MatchId)))
+            .ToListAsync();
+    }
+
+    private List<ApplicationUser> ProcessPredictions(List<ApplicationUser> users, List<MatchResult> finishedMatches)
+    {
+        var usersToUpdate = new List<ApplicationUser>();
 
         foreach (var user in users)
         {
-            var prediction = user.Predictions.FirstOrDefault(x => x.MatchId == matchResult.MatchId);
-
-            if (prediction is null)
-                continue;
-
-            prediction.PointPerMatch = GetUserPoint();
-            prediction.Coefficient = GetCoefficient();
-
-            await userRepository.UpdateAsync(user);
-
-            #region local methods
-
-            int GetUserPoint()
+            foreach (var prediction in user.Predictions)
             {
-                var (homeTeamScore, awayTeamScore) = GetScore();
+                var matchResult = finishedMatches.FirstOrDefault(x => x.MatchId == prediction.MatchId);
 
-                var isPredictedWinner = (homeTeamScore > awayTeamScore && prediction.PredictedHomeScore > prediction.PredictedAwayScore)
-                                        || (homeTeamScore < awayTeamScore && prediction.PredictedHomeScore < prediction.PredictedAwayScore)
-                                        || (homeTeamScore == awayTeamScore && prediction.PredictedHomeScore == prediction.PredictedAwayScore);
+                if (matchResult is null)
+                    continue;
 
-                if (!isPredictedWinner)
-                    return 0;
+                prediction.PointPerMatch = GetUserPoint(matchResult, prediction);
+                prediction.Coefficient = GetCoefficient(matchResult);
 
-                var userPoint = settings.PointSettings.Outcome; // Балл за правильный исход
-                
-                if (homeTeamScore - awayTeamScore == prediction.PredictedHomeScore - prediction.PredictedAwayScore)
-                {
-                    userPoint = settings.PointSettings.GoalDifference; // Балл исход + за правильную разницу забитых мячей
-                }
-
-                if (homeTeamScore == prediction.PredictedHomeScore && awayTeamScore == prediction.PredictedAwayScore)
-                {
-                    userPoint = settings.PointSettings.ExactScore; // Балл за точный счет
-                }
-
-                return userPoint;
+                if (!usersToUpdate.Contains(user))
+                    usersToUpdate.Add(user);
             }
-
-            double GetCoefficient()
-            {
-                return matchResult.Match.Stage switch
-                {
-                    "LEAGUE_STAGE" => settings.CoefficientSettings.LeagueStage,
-                    "LAST_16" => settings.CoefficientSettings.Last16,
-                    "QUARTER_FINALS" => settings.CoefficientSettings.QuarterFinals,
-                    "SEMI_FINALS" => settings.CoefficientSettings.SemiFinals,
-                    "FINAL" => settings.CoefficientSettings.Final,
-                    _ => settings.CoefficientSettings.LeagueStage
-                };
-            }
-
-            (int HomeTeamScore, int AwayTeamScore) GetScore()
-            {
-                if (matchResult.Duration is DurationStatus.ExtraTime or DurationStatus.PenaltyShootout)
-                {
-                    var regularTime = matchResult.RegularTime.Split(":");
-
-                    return (Convert.ToInt32(regularTime[0]), Convert.ToInt32(regularTime[1]));
-                }
-
-                var fullTime = matchResult.FullTime.Split(":");
-
-                return (Convert.ToInt32(fullTime[0]), Convert.ToInt32(fullTime[1]));
-            }
-
-            #endregion
         }
+
+        return usersToUpdate;
+    }
+
+    private int GetUserPoint(MatchResult matchResult, Prediction prediction)
+    {
+        var (homeTeamScore, awayTeamScore) = GetScore(matchResult);
+
+        var isPredictedWinner = (homeTeamScore > awayTeamScore && prediction.PredictedHomeScore > prediction.PredictedAwayScore)
+                                || (homeTeamScore < awayTeamScore && prediction.PredictedHomeScore < prediction.PredictedAwayScore)
+                                || (homeTeamScore == awayTeamScore && prediction.PredictedHomeScore == prediction.PredictedAwayScore);
+
+        if (!isPredictedWinner)
+            return 0;
+
+        var userPoint = settings.PointSettings.Outcome; // Балл за правильный исход
+
+        if (homeTeamScore - awayTeamScore == prediction.PredictedHomeScore - prediction.PredictedAwayScore)
+        {
+            userPoint = settings.PointSettings.GoalDifference; // Балл за правильную разницу забитых мячей
+        }
+
+        if (homeTeamScore == prediction.PredictedHomeScore && awayTeamScore == prediction.PredictedAwayScore)
+        {
+            userPoint = settings.PointSettings.ExactScore; // Балл за точный счет
+        }
+
+        return userPoint;
+    }
+
+    private double GetCoefficient(MatchResult matchResult)
+    {
+        return matchResult.Match.Stage switch
+        {
+            "LEAGUE_STAGE" => settings.CoefficientSettings.LeagueStage,
+            "LAST_16" => settings.CoefficientSettings.Last16,
+            "QUARTER_FINALS" => settings.CoefficientSettings.QuarterFinals,
+            "SEMI_FINALS" => settings.CoefficientSettings.SemiFinals,
+            "FINAL" => settings.CoefficientSettings.Final,
+            _ => settings.CoefficientSettings.LeagueStage
+        };
+    }
+
+    private static (int HomeTeamScore, int AwayTeamScore) GetScore(MatchResult matchResult)
+    {
+        int homeTeamScore;
+        int awayTeamScore;
+
+        if (matchResult.Duration is DurationStatus.ExtraTime or DurationStatus.PenaltyShootout)
+        {
+            var regularTime = matchResult.RegularTime.Split(":");
+            homeTeamScore = int.Parse(regularTime[0]);
+            awayTeamScore = int.Parse(regularTime[1]);
+            
+            return (homeTeamScore, awayTeamScore);
+        }
+
+        var fullTime = matchResult.FullTime.Split(":");
+        homeTeamScore = int.Parse(fullTime[0]);
+        awayTeamScore = int.Parse(fullTime[1]);
+        
+        return (homeTeamScore, awayTeamScore);
     }
 }
