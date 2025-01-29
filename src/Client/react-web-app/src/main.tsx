6340af1df02868fc619805dead2daddb41a618fe
Kiryl Kisly <kiryl.kisly@gmail.com>
2025-01-29 23:50:59 +0300

    fix: Add wrap in SDKProvider

--- src/Client/react-web-app/src/main.tsx
+++ src/Client/react-web-app/src/main.tsx
@@ -1,10 +1,14 @@
+import { SDKProvider } from '@telegram-apps/sdk-react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './components/App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <SDKProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SDKProvider>
+
 )
