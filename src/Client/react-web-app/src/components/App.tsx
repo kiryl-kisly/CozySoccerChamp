8074a77c3f537d3f6031700adf1ae8f732672bca
Kiryl Kisly <kiryl.kisly@gmail.com>
2025-01-30 01:19:44 +0300

    Revert "fix: remove async from app.tsx"
    This reverts commit 8c29be8cebb78bf857613a67a4f622e0a983ddbb.

--- src/Client/react-web-app/src/components/App.tsx
+++ src/Client/react-web-app/src/components/App.tsx
@@ -15,7 +15,7 @@ import './App.css'
 import { HeaderBar } from './HeaderBar/HeaderBar'
 import { Menu } from './Menu/Menu'
 
-export function App() {
+export async function App() {
   const [data, setData] = useState<IInitDataResponse>({
     isLoading: true,
     competition: null,
