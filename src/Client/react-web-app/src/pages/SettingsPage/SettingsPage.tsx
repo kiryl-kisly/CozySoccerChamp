127ca24052e2c4507ae7728aaa914c2e53aba355
Kiryl Kisly <kiryl.kisly@gmail.com>
2025-02-23 16:00:19 +0300

    fix: Remove unused code

--- src/Client/react-web-app/src/pages/SettingsPage/SettingsPage.tsx
+++ src/Client/react-web-app/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,67 +1,8 @@
-import { useEffect, useState } from 'react'
 import { IoCodeWorking, IoEarthSharp, IoFolder, IoNotifications, IoPerson } from 'react-icons/io5'
 import { SettingItem } from '../../components/Setting/SettingItem'
 import './SettingsPage.css'
 
 export function SettingsPage() {
-	const [isInAppVibrate, setIsInAppVibrate] = useState<boolean>(() => {
-		const saved = localStorage.getItem('isInAppVibrate')
-		return saved !== null ? JSON.parse(saved) : false
-	})
-
-	const [isHideFinishedMatches, setIsHideFinishedMatches] = useState<boolean>(() => {
-		const saved = localStorage.getItem('isHideFinishedMatches')
-		return saved !== null ? JSON.parse(saved) : false
-	})
-
-	useEffect(() => {
-		localStorage.setItem('isHideFinishedMatches', JSON.stringify(isHideFinishedMatches))
-		localStorage.setItem('isInAppVibrate', JSON.stringify(isInAppVibrate))
-	}, [isHideFinishedMatches, isInAppVibrate])
-
-	const toggleIsHideFinishedMatches = () => {
-		setIsHideFinishedMatches(!isHideFinishedMatches)
-	}
-	const toggleIsInAppVibrate = () => {
-		setIsInAppVibrate(!isInAppVibrate)
-	}
-
-	// return (
-	// 	<>
-	// 		<h1 className='title-page'>Settings</h1>
-
-	// 		<p className='text-gray-400 font-thin'>IN-APP NOTIFICATIONS</p>
-
-
-	// 		{/* Включить/выключить haptic */}
-	// 		<div
-	// 			className='flex justify-between items-center ml-5 mr-5 text-lg'
-	// 			onClick={toggleIsInAppVibrate}>
-	// 			<div className='absolute left-5 p-2'>In-App Vibrate</div>
-	// 			<div className='ml-auto p-2'>
-	// 				<div className={`toggle ${isInAppVibrate ? 'toggle-active-color' : 'toggle-disable-color'}`}>
-	// 					<div className={`toggle-circle ${isInAppVibrate ? 'translate-x-8' : 'translate-x-0'}`}>
-	// 					</div>
-	// 				</div>
-	// 			</div>
-	// 		</div>
-
-	// 		<p className='text-gray-400 font-thin mt-6'>OTHER</p>
-	// 		{/* Скрыть/показать завершенные матчи */}
-	// 		<div
-	// 			className='flex justify-between items-center ml-5 mr-5 text-lg'
-	// 			onClick={toggleIsHideFinishedMatches}>
-	// 			<div className='absolute left-5 p-2'>Hide Finished Matches</div>
-	// 			<div className='ml-auto p-2'>
-	// 				<div className={`toggle ${isHideFinishedMatches ? 'toggle-active-color' : 'toggle-disable-color'}`}>
-	// 					<div className={`toggle-circle ${isHideFinishedMatches ? 'translate-x-8' : 'translate-x-0'}`}>
-	// 					</div>
-	// 				</div>
-	// 			</div>
-	// 		</div>
-	// 	</>
-	// )
-
 	return (
 		<>
 			<h1 className='title-page'>Settings</h1>
